845b59275175eedac448211929d36ed7cbf22798
shilovapaulina <shilovapaulina@gmail.com>
2025-11-22 13:36:17 -0500

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -20,7 +20,7 @@ df = get_data()
 
 # ---------------------- CLEANING ---------------------- #
 
-type_map = {
+type_map = 
     "townhouse": "Townhouse",
     "condop": "Condop",
     "condo": "Condo",
@@ -281,3 +281,4 @@ st.dataframe(display_price[[
     "ADDRESS"
 
 ]])
+
